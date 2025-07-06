304fdc3e1e401e7433f694aae249b6fa082bd456
KittymasterHehe <kittymaster103192@gmail.com>
2025-07-06 11:52:01 -0400

    Update pyritetoiron.js

--- mods/pyritetoiron.js
+++ mods/pyritetoiron.js
@@ -1,10 +1,23 @@
 // pyrite_centrifuge_mod.js
 
-elements.centrifuge_fluid = {
-	color: "#ff0000",
-	behavior: behaviors.LIQUID,
-	category: "liquids",
-	state: "liquid",
+elements.methanol = {
+    color: "#c9c5b1",
+    behavior: behaviors.LIQUID,
+    reactions: {
+        "virus": { "elem2":null },
+        "plague": { "elem2":null },
+        "head": { "elem2":"rotten_meat", "chance": 0.8 },
+        "body": { "elem2":"rotten_meat", "chance": 0.8 },
+    },
+    viscosity: 0.56,
+    //tempHigh: 64.7,
+    burn: 100,
+    burnTime: 2,
+    fireColor: "#b2c5d1",
+    category: "liquids",
+    state: "liquid",
+    density: 792,
+    stain: -0.25,
 }
 
 
